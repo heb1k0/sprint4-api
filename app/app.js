60732fc6d48f60916316741ec3237060b03e9da3
Alejandro <alejandrozuriguel@gmail.com>
2021-12-14 17:25:54 +0100

    changes login

--- app/app.js
+++ app/app.js
@@ -4,12 +4,11 @@ const routerFiles = require("./routes/upload");
 const routerUsers = require("./routes/users");
 
 const cors = require('./middleware/cors');
-const login = require('./middleware/login');
 
 app.get('/', (req, res) => {res.json({statusAPI: "API is running"})});
 app.use(express.json());
 app.use(routerFiles)
-app.use(login.islogin,cors.cacheController,routerUsers)
+app.use(cors.cacheController,routerUsers)
 
 
 
--- app/routes/users.js
+++ app/routes/users.js
@@ -2,8 +2,10 @@ const controllers  = require("../controllers/UserController");
 const express = require('express');
 const router =  express.Router();
 
+const login = require('../middleware/login');
+
 router.get('/user', controllers.userController);
-router.post('/time', controllers.timeController);
+router.post('/time', login.islogin, controllers.timeController);
 
 
 module.exports = router;
